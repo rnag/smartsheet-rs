c5cef557f0eacdf46b9baeb291df7345622177d0
Ritvik Nag <nagrim7@gmail.com>
2022-02-10 01:02:10 -0500

    Fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -39,18 +39,35 @@
 //! - [List Columns](https://smartsheet-platform.github.io/api-docs/#list-columns)
 //! - [Get Sheet](https://smartsheet-platform.github.io/api-docs/#get-sheet)
 //! - [Get Row](https://smartsheet-platform.github.io/api-docs/#get-row)
+//! - [Get Column](https://smartsheet-platform.github.io/api-docs/#get-column)
 //!
 //! You can check out sample usage of these API methods in the [examples/](https://github.com/rnag/smartsheet-rs/tree/main/examples)
 //! folder in the project repo on GitHub.
 //!
-//! ## Dependencies
+//! ## Dependencies and Features
 //!
 //! This library uses only the minimum required dependencies, in order
 //! to keep the overall size small. This crate uses [hyper][] and
 //! [hyper-rustls][] internally, to make HTTPS requests to the Smartsheet API.
 //!
+//! While `hyper-rustls` was chosen as the default TLS implementation
+//! because it works without issue when cross-compiling for the
+//! **x86_64-unknown-linux-musl** target as is common for [AWS Lambda][]
+//! deployments, it is still possible to instead use the native [`hyper-tls`][]
+//! implementation, which relies on OpenSSL.
+//!
+//! To do this, disable the default "rust-tls" feature and enable the "native-tls" feature:
+//!
+//! ```toml
+//! [dependencies]
+//! smartsheet-rs = { version = "0.2", default-features = false, features = ["native-tls", "logging", "serde-std"] }
+//! ```
+//!
 //! [hyper]: https://docs.rs/hyper
 //! [hyper-rustls]: https://docs.rs/hyper-rustls
+//! [`hyper-tls`]: https://docs.rs/hyper-tls
+//! [AWS Lambda]: https://docs.aws.amazon.com/sdk-for-rust/latest/dg/lambda.html
+//!
 
 // #![warn(missing_docs)]
 
